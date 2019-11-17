30f33c5c19a3b618a6066362232ffa9e97974cbf
archit <architsharma846@gmail.com>
2019-11-17 23:38:37 +0530

    our first commit

---
+++ public/browser.js
@@ -0,0 +1,45 @@
+function itemTemplate(item){
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+  </li>`
+}
+let list_item=document.getElementById("list_item");
+let createField=document.getElementById("create_field");
+let ourHTML=items.map((item)=>{
+return itemTemplate(item);
+}).join('');
+list_item.insertAdjacentHTML("beforeend",ourHTML);
+document.getElementById("create_form").addEventListener("submit",(e)=>{
+e.preventDefault();
+axios.post('/create_item',{text:createField.value}).then((response)=>{
+   document.getElementById("list_item").insertAdjacentHTML("beforeend",itemTemplate(response.data))
+}).catch(()=>{
+    console.log("please try again");
+});
+})
+
+document.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("delete-me"))
+    {
+        if(confirm("Do you want to delete it ?")){
+        axios.post('/delete_item',{id:e.target.getAttribute("data-id")}).then(()=>{
+            e.target.parentElement.parentElement.remove();
+        }).catch(()=>{
+            console.log("please try again");
+        });
+    }
+    }
+    if(e.target.classList.contains("edit-me"))
+    {
+        let userInput=prompt("enter your desired new text",e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
+        axios.post('/update_item',{text:userInput,id:e.target.getAttribute("data-id")}).then(()=>{
+            e.target.parentElement.parentElement.querySelector(".item-text").innerHTML=userInput;
+        }).catch(()=>{
+            console.log("please try again");
+        });
+    }
+})
